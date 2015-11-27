8e8752b3a538650f2e1b07759cea0900aae6672f
Jorge Reig <reig.aguado@gmail.com>
2015-11-27 14:00:40 +0100

    Permanent log in working

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   
 
   root 'static_pages#home'
-
+   get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   get 'help' => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
